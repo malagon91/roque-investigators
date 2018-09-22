40ea2241fd35512247065f0bda4c059639cb4689
Unknown <miguelmalagon@outlook.com>
2018-09-22 17:36:46 -0500

    Mysql connection works

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/investigators', function(req, res, next) {
+  res.locals.connection.query('SELECT * from Investigator', function (error, results, fields) {
+		if(error) throw error;
+		res.send(JSON.stringify(results));
+	});
+});
+
+module.exports = router;
