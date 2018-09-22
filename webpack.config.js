140b5e7eccb5f69a974f2f97751b9dd80c80b180
Unknown <miguelmalagon@outlook.com>
2018-09-22 17:45:31 -0500

    React works

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {// add babel polyfill to run most new code on js
+  resolve: {
+    modules:[
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+  entry: ['babel-polyfill','./lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname,'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader' }
+    ]
+  }
+};// yarn add @babel/env @babel/react @babel/preset-react
