302dffb18766b50207a75c6943b0e8d964d04818
Miguel Malagon <miguelmalagon@outlook.com>
2018-09-26 18:37:41 -0500

    correct routes in react and node

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/investigators', function(req, res, next) {
   res.locals.connection.query('SELECT * from Investigator', function (error, results, fields) {
