ffafa8672fa0df61cf3e38d3797389c2d71429ad
Unknown <miguelmalagon@outlook.com>
2018-10-14 23:51:21 -0500

    refactor form

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import sharp from 'sharp';
 import fs from 'fs';
 import config from './../lib/config';
 import middleware from './../lib/middleware';
+import {checkIffileExists} from './../lib/utils';
 
 var router = express.Router();
 
@@ -24,7 +25,9 @@ router.get('/investigator/:id',middleware.checkToken, function(req, res) {
 			object_error["message"] = 'Error getting the investigator';
 			res.status(500).json(object_error);
 		}else{
-			res.status(200).json(results[0]);
+			const existImage = checkIffileExists(`${config.profile_photo_path}${results[0].Id}_profile.jpg`);
+			const resume = {...results[0], existImage: existImage};
+			res.status(200).json(resume);
 		}
 	});
 });
@@ -49,6 +52,7 @@ router.post('/user',middleware.checkToken, function(req,res){
 router.put('/user',middleware.checkToken, function(req,res){
 	let appData = {};
 	const user = req.body;
+	console.log(user);
 	res.locals.connection.query("update Investigator set ? where Id = ?", [user, user.Id], function(error,results,fields){
 		if (error){
 			appData = {success:false, message: "Error to update the user"};
@@ -129,7 +133,9 @@ router.post('/login', function(req,res){
 						appData = {success: true, message: "Login successfully"};
 						userInfo = {...userInfo, token: token}
 						let resume = results[0];
+						const existImage = checkIffileExists(`${config.profile_photo_path}${results[0].Id}_profile.jpg`);
 						delete resume.Password_;
+						resume = {...resume, existImage: existImage};
 						appData = {...appData, userInfo, resume: resume }
 						res.status(200).json(appData);
 					}else{
